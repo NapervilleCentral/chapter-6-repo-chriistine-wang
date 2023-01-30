28f441c478a72f7e57d10057857709c9187bcce9
chriistine-wang <qwang2@stu.naperville203.org>
2023-01-30 14:14:06 -0600

    erfe

---
+++ Intro Code/MagicSquare.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class MagicSquare here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MagicSquare
+{
+     
+    public MagicSquare(int [] [] array)
+    {
+        
+    }
+    
+    public boolean isMagic()
+    {
+        
+    }
+    
+    public int getMagicNum()
+    {
+        
+    }
+    
+    public void setMagicSquare(int [] [] array)
+    {
+        
+    }
+    // support methods
+    
+    public int addRows(int row)
+    {
+        
+    }
+    
+    public int[] addRows()
+    {
+        
+    }
+    
+    public int addColumn(int col)
+    {
+        
+    }
+    
+    public int[] addColumns()
+    {
+        
+    }
+    
+}
