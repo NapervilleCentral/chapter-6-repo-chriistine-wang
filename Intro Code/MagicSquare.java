60686b451e6b132e364b5a8947c8b383d17b9935
chriistine-wang <qwang2@stu.naperville203.org>
2023-02-01 00:15:29 -0600

    wddwwd

--- Intro Code/MagicSquare.java
+++ Intro Code/MagicSquare.java
@@ -8,20 +8,25 @@
 public class MagicSquare
 {
     private int[][] square;
+    private int magicNum;
+    private int numRows, numCols;
     
     public MagicSquare(int [][] array)
     {
         square = array;
+        numRows = array.length;
+        numCols = array[0].length;
     }
     
     public boolean isMagic()
     {
         
+        
     }
     
-    public int getMagicNum()
+    public int getMagicNum()// gets the magic number of 1 row/col/diag, etc
     {
-        
+        return magicNum;
     }
     
     public void setMagicSquare(int [][] array)
@@ -32,21 +37,39 @@ public class MagicSquare
     
     public int addRow(int row)
     {
-        
+        int rowSum = 0;
+        for (int col = 0; col < numRows; col++)
+        {
+            rowSum += square[row][col];
+        }
+        return rowSum;
     }
     
     public int[] addRows()
     {
-        
+        int cache = 0;
+        for (int i = 0; i < numRows; i++)
+        {
+            if (addRow(i) == cache)
+            {
+                
+            }
+            else
+            {
+                
+            }
+            cache = this.addRow(i);
+            
+        }
     }
     
     public int addColumn(int col)
     {
-        int sum = 0;
+        int colSum = 0;
         for (int i = 0; i < square.length-1; i++)
         {
-            sum += square[i][col];
-            
+            colSum += square[i][col];
+    
         }
     }
     
